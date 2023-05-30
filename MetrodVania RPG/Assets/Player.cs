552dd0646bec91e0a220111ec0085e9c84304a13
cauerh <106555565+cauerh@users.noreply.github.com>
2023-05-30 16:03:59 -0300

    add jump and air states

--- MetrodVania RPG/Assets/Player.cs
+++ MetrodVania RPG/Assets/Player.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    [Header("MOve Info")]
+    [Header("Move Info")]
     public float moveSpeed = 12f;
+    public float jumpForce;
 
     #region Components
     public Animator anim { get; private set; }
@@ -19,6 +20,8 @@ public class Player : MonoBehaviour
     public PlayerIdleState idleState { get; private set; }
 
     public PlayerMoveState moveState { get; private set; }
+    public PlayerJumpState jumpState { get; private set; }
+    public PlayerAirState airState { get; private set; }
 
     #endregion
 
@@ -28,6 +31,8 @@ public class Player : MonoBehaviour
 
         idleState = new PlayerIdleState(this, stateMachine, "Idle");
         moveState = new PlayerMoveState(this, stateMachine, "Move");
+        jumpState = new PlayerJumpState(this, stateMachine, "Jump");
+        airState = new PlayerAirState(this, stateMachine, "Jump");
     }
 
     private void Start()

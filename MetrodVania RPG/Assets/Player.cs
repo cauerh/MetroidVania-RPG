d6abf58500360e1498e0ee2545cdad10c5374ad7
cauerh <106555565+cauerh@users.noreply.github.com>
2023-05-30 21:19:00 -0300

    add dash state

--- MetrodVania RPG/Assets/Player.cs
+++ MetrodVania RPG/Assets/Player.cs
@@ -9,6 +9,23 @@ public class Player : MonoBehaviour
     public float moveSpeed = 12f;
     public float jumpForce;
 
+    [Header("Dash Info")]
+    [SerializeField] private float dashCooldown;
+    private float dashUsageTimer;
+    public float dashSpeed;
+    public float dashDuration;
+    public float dashDir { get; private set; }
+
+    [Header("Collision Info")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private float wallCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+
+    public int facingDir { get; private set; } = 1;
+    private bool facingRight = true;
+
     #region Components
     public Animator anim { get; private set; }
     public Rigidbody2D rb { get; private set; }
@@ -23,6 +40,8 @@ public class Player : MonoBehaviour
     public PlayerJumpState jumpState { get; private set; }
     public PlayerAirState airState { get; private set; }
 
+    public PlayerDashState dashState { get; private set; }
+
     #endregion
 
     private void Awake()
@@ -32,7 +51,8 @@ public class Player : MonoBehaviour
         idleState = new PlayerIdleState(this, stateMachine, "Idle");
         moveState = new PlayerMoveState(this, stateMachine, "Move");
         jumpState = new PlayerJumpState(this, stateMachine, "Jump");
-        airState = new PlayerAirState(this, stateMachine, "Jump");
+        airState  = new PlayerAirState(this, stateMachine, "Jump");
+        dashState = new PlayerDashState(this, stateMachine, "Dash"); 
     }
 
     private void Start()
@@ -43,13 +63,57 @@ public class Player : MonoBehaviour
         stateMachine.Initialize(idleState);
     }
 
+
     private void Update()
     {
         stateMachine.currentState.Update();
+           
+        CheckForDashInput();
     }
 
-    public void SetVelocity(float _xvelocity, float _yVelocity)
+    private void CheckForDashInput()
     {
-        rb.velocity = new Vector2(_xvelocity, _yVelocity);
+        dashUsageTimer -= Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && dashUsageTimer < 0)
+        {
+            dashUsageTimer = dashCooldown;
+            dashDir = Input.GetAxisRaw("Horizontal");
+
+            if(dashDir == 0)
+                dashDir = facingDir;
+
+            stateMachine.ChangeState(dashState);
+        }
+           
+    }
+
+    public void SetVelocity(float _xVelocity, float _yVelocity)
+    {
+        rb.velocity = new Vector2(_xVelocity, _yVelocity);
+        FlipController(_xVelocity);
+    }
+
+    public bool IsGroundDetected() => Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+    }
+
+    public void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    public void FlipController(float _x)
+    {
+        if(_x > 0 && !facingRight)
+            Flip();
+        else if (_x < 0 && facingRight)
+            Flip();
     }
 }

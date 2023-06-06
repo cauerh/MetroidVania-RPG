93efcdc1bd20b4d2368de9c6039caae3decd5f7f
cauerh <106555565+cauerh@users.noreply.github.com>
2023-06-06 15:22:25 -0300

    create battle state

--- MetrodVania RPG/Assets/Scripts/Enemy.cs
+++ MetrodVania RPG/Assets/Scripts/Enemy.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Enemy : Entity
 {
+    [SerializeField] protected LayerMask whatIsPlayer;
     [Header("Move Info")]
     public float moveSpeed;
     public float idleTime;
+
+    [Header("Attack Info")]
+    public float attackDistance;
+
 
     public EnemyStateMachine stateMachine { get; private set; }
 
@@ -21,5 +26,16 @@ public class Enemy : Entity
         base.Update();
 
         stateMachine.currentState.Update();
+
+    }
+
+    public virtual RaycastHit2D isPlayerDetected() => Physics2D.Raycast(wallCheck.position, Vector2.right * facingDir, 50, whatIsPlayer);
+
+    protected override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x + attackDistance * facingDir, transform.position.y));
     }
 }

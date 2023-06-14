a12f1690132e9945bfcad5d7303c74bc9d18853f
cauerh <106555565+cauerh@users.noreply.github.com>
2023-06-14 20:22:03 -0300

    player counter attack

--- MetrodVania RPG/Assets/Scripts/Enemy/Skeleton/Enemy_Skeleton.cs
+++ MetrodVania RPG/Assets/Scripts/Enemy/Skeleton/Enemy_Skeleton.cs
@@ -42,7 +42,7 @@ public class Enemy_Skeleton : Enemy
         }
     }
 
-    protected override bool CanBeStunned()
+    public override bool CanBeStunned()
     {
         if (base.CanBeStunned())
         {
